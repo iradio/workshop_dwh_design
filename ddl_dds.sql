44920e9f453d12ec02900a9f287b6606392c96d0
Larisa <larisfdz@gmail.com>
2022-09-09 11:07:20 +0300

    added constraints

--- ddl_dds.sql
+++ ddl_dds.sql
@@ -1,39 +1,40 @@
 -- DDS
 create schema dev_dds;
 
-drop table if exists dev_dds.d_product;
+drop table if exists dev_dds.d_product cascade;
 
 create table if not exists dev_dds.d_product (
 	id serial primary key,
-	product varchar(50)
+	product varchar(50) NOT NULL 
 );
 
-drop table if exists dev_dds.d_sale_point;
+drop table if exists dev_dds.d_sale_point cascade;
 
 create table if not exists dev_dds.d_sale_point (
 	id serial primary key,
-	sales_point varchar(50)
+	sales_point varchar(50) NOT NULL
 );
 
 drop table if exists dev_dds.f_plan;
 
 create table if not exists dev_dds.f_plan (
-	id serial,
+	id serial PRIMARY key,
 	sales_point_id int4 references dev_dds.d_sale_point (id),
 	product_id int4 references dev_dds.d_product (id),
-	year_month varchar,
---	plan_revenue_amount,
-	price decimal(10,2),
-	sale_cnt int
+	--предлагаю все же разделить, чтобы, при необходимости, быстро создать дату
+	"year" SMALLINT NOT NULL CONSTRAINT f_plan_year_check CHECK (("year">=2022) AND ("year"<2500)),
+	"month" SMALLINT NOT NULL CONSTRAINT f_plan_month_check CHECK (("month">=1) AND ("month"<=12)),
+	price decimal(10,2) DEFAULT 0 NOT NULL CONSTRAINT f_sales_price_check CHECK (price >= 0),
+	sale_cnt int DEFAULT 0 NOT NULL CONSTRAINT f_plan_sales_cnt_check CHECK ((sale_cnt >= 0))
 );
 
 drop table if exists dev_dds.f_sales;
 
 create table if not exists dev_dds.f_sales (
-	id serial,
+	id serial PRIMARY key,
 	sales_point_id int4 references dev_dds.d_sale_point (id),
 	product_id int4 references dev_dds.d_product (id),
-	"date" date,
-	price decimal(10,2),
-	sale_cnt int
+	"date" date NOT NULL,
+	price decimal(10,2) DEFAULT 0 NOT NULL CONSTRAINT f_sales_price_check CHECK (price >= 0),
+	sale_cnt int DEFAULT 0 NOT NULL CONSTRAINT f_sales_sales_cnt_check CHECK ((sale_cnt >= 0))
 );
--- fill_dds.sql
+++ fill_dds.sql
@@ -11,18 +11,20 @@ select distinct sales_point from dev_stg.plan_2022;
 insert into dev_dds.f_plan (	
 sales_point_id,
 product_id,
-year_month,
+"year",
+"month",
 price,
 sale_cnt)
 select 
 	dsp.id sales_point_id,
 	dp.id product_id,
-	concat('2022-',p."month_")  year_month,
+	2022 "year",
+	p."month_" "month",
 	p.price price,
 	p.sale_cnt sale_cnt 
 from dev_stg.plan_2022 p 
 left join dev_dds.d_sale_point dsp on p.sales_point = dsp.sales_point 
-left join dev_dds.d_product dp on p.product = dp.product 
+left join dev_dds.d_product dp on p.product = dp.product; 
 
 insert into dev_dds.f_sales (	
 sales_point_id,
